adf1d56f09ae5cdfa4662cc02bc3c32f99d65abd
SUAREZ SOSA <j.s.sosa@cimmyt.org>
2020-03-20 17:00:20 -0600

    Add hook form and validation

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,38 +1,74 @@
-import React from "react";
+import React from 'react';
+import useFormValidation from './useFormValidation';
+import validateLogin from './validateLogin';
 
 function Login(props) {
-  const [login, setLogin] = React.useState(true)
+  const intialState = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  const [login, setLogin] = React.useState(true);
+
+  const {
+    handleSubmit,
+    handleBlur,
+    handleChange,
+    values,
+    errors,
+    isSubmitting,
+  } = useFormValidation(intialState, validateLogin);
 
   return (
     <div>
-      <h2 className="mv3">{login ? "Login" : "Create Account" }</h2>
-      <form className="flex flex-column">
-        {
-          !login &&
-          <input 
-            type='text'
-            placeholder='Your name'
-            autocomplete='off'
+      <h2 className="mv3">{login ? 'Login' : 'Create Account'}</h2>
+      <form className="flex flex-column" onSubmit={handleSubmit}>
+        {!login && (
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            placeholder="Your name"
+            className={errors.email && 'error-input'}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
-        }
-        <input 
-          type='email'
-          placeholder='Your email'
-          autocomplete='off'
+        )}
+        <input
+          type="email"
+          name="email"
+          value={values.email}
+          placeholder="Your email"
+          className={errors.password && 'error-input'}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
-        <input 
-          type='password'
-          placeholder='Choose a secure password'
+        {errors.email && <p className='error-text'> {errors.email} </p>}
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          placeholder="Choose a secure password"
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {errors.password && <p className='error-text'> {errors.password} </p>}
         <div className="flex mt3">
-          <button type="submit" className="button pointer mr2">submit</button>
-          <button type="button" className="pointer button" onClick={() => setLogin( prevLogin => !prevLogin )}>
-            { login ? "need to create an account" : "already have an account"}
+          <button type="submit" className="button pointer mr2" disabled={isSubmitting} style={{ background: isSubmitting? 'gray': 'orange'}}>
+            submit
+          </button>
+          <button
+            type="button"
+            className="pointer button"
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+          >
+            {login ? 'need to create an account' : 'already have an account'}
           </button>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
 export default Login;
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,57 @@
-import React from "react";
+import { useState, useEffect } from 'react'
 
-function useFormValidation() {}
+export default function useFormValidation(initialState, validate) {
 
-export default useFormValidation;
+    const [values, setValues] = useState(initialState)
+    const [errors, setErrors] = useState({})
+    const [isSubmitting , setSubmitting] = useState(false)
+
+    useEffect(() => {
+        if(isSubmitting){
+            const noErrors = Object.keys(errors).length === 0
+            if(noErrors){
+                console.log('authenticated', values)
+                setSubmitting(false)
+            } else {
+                setSubmitting(false)
+            }
+        }
+
+    }, [errors])
+
+    const handleChange = (event) => {
+        event.persist()
+
+        setValues(
+            previousValues => (
+                {
+                ...previousValues,
+                [event.target.name] : event.target.value
+
+                }
+            )
+        )
+    }
+
+    const handleBlur = () => {
+        const validationErros = validate(values)
+        setErrors(validationErros)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const validationErros = validate(values)
+        setErrors(validationErros)
+        setSubmitting(true)
+    }
+
+    return {
+        handleSubmit,
+        handleBlur,
+        handleChange,
+        values,
+        errors,
+        isSubmitting
+    }
+    
+}
